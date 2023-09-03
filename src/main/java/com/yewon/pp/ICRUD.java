cae7d7c1636b655c7d0e892666f41572f4fe361e
KangYewon <ooooo712@naver.com>
2023-09-03 23:41:23 +0900

    R과 C기능 중 1번 기능까지 완료! 별도로 출력 동일하게 세심한 부분 확인 필요

--- src/main/java/com/yewon/pp/ICRUD.java
+++ src/main/java/com/yewon/pp/ICRUD.java
@@ -1,5 +1,8 @@
 package com.yewon.pp;
 
 public interface ICRUD {
-    public Object add();
+    public Object add(); // data 추가할 때 사용
+    public int update(Object obj);
+    public int delete(Object obj);
+    public void selectOne(int id); //data 한 개 조회
 }
--- src/main/java/com/yewon/pp/WordCRUD.java
+++ src/main/java/com/yewon/pp/WordCRUD.java
@@ -1,4 +1,55 @@
 package com.yewon.pp;
+import java.util.ArrayList;
+import java.util.Scanner;
+//WordCRUD는 ICRUD를 구현한 구현체이다.
 
-public class WordCRUD {
+public class WordCRUD implements ICRUD{
+    ArrayList<Word> list;
+    Scanner s;
+
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s = s;
+    }
+    @Override
+    public Object add() { // 사용자에게 입력받는 부분
+        System.out.print("==> 난이도(1,2,3) & 새 단어 입력 : \n");
+        int level = s.nextInt();
+        String word = s.nextLine();
+
+        System.out.print("뜻 입력 : ");
+        String meaning = s.nextLine();
+
+        return new Word(0, level, word, meaning);
+    }
+
+    public void addWord() { // 입력받은 데이타를 리스트에 추가하는 부분
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다 !!! ");
+    }
+
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public void selectOne(int id) {
+
+    }
+
+    public void listAll(){
+        System.out.println("---------------------");
+        for(int i=0; i<list.size(); i++ ){
+            System.out.print((i+1)+ " ");
+            System.out.println(list.get(i).toString());
+        }
+        System.out.println("---------------------");
+    }
 }
