ffe55a2c9868761cb06933cc6538d013dfc51adf
KangYewon <ooooo712@naver.com>
2023-09-13 21:25:29 +0900

    코드 정리

--- src/main/java/com/yewon/pp/ICRUD.java
+++ src/main/java/com/yewon/pp/ICRUD.java
@@ -2,7 +2,11 @@ package com.yewon.pp;
 
 public interface ICRUD {
     public Object add(); // data 추가할 때 사용
+    /*
+
     public int update(Object obj);
     public int delete(Object obj);
-    public void selectOne(int id); //data 한 개 조회
+    public void select(int id); //data 한 개 조회
+
+     */
 }
--- src/main/java/com/yewon/pp/WordCRUD.java
+++ src/main/java/com/yewon/pp/WordCRUD.java
@@ -33,21 +33,6 @@ public class WordCRUD implements ICRUD{
         System.out.println("새 단어가 단어장에 추가되었습니다 !!! \n");
     }
 
-    @Override
-    public int update(Object obj) {
-        return 0;
-    }
-
-    @Override
-    public int delete(Object obj) {
-        return 0;
-    }
-
-    @Override
-    public void selectOne(int id) {
-
-    }
-
     public void listAll(){
         System.out.println("---------------------");
         for(int i=0; i<list.size(); i++ ){
